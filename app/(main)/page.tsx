980ce891139af99f9e582f3403b2218317973008
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-03-20 07:26:02 +0700

    fix the Canonical link

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,13 +3,21 @@ import Hero from "./_components/Hero";
 import MainFaq from "./_components/mainFaq";
 
 import ServicesSection from "./_components/Services";
-import { createMetadata, viewport } from '@/lib/metadata';
+import { createMetadata, viewport } from "@/lib/metadata";
 
 export const metadata = createMetadata({
-  title: 'Bulba Cloud - Layanan Hosting & Domain Terpercaya di Indonesia',
-  description: 'Penyedia layanan hosting terbaik di Indonesia dengan uptime 99.9%, domain murah, VPS performa tinggi, dan solusi website untuk semua kebutuhan bisnis Anda.',
-  path: '/',
-  keywords: ['vps hosting', 'cloud hosting', 'minecraft hosting', 'jasa pembuatan website', 'domain murah', 'vps digital ocean'],
+  title: "Bulba Cloud - Layanan Hosting & Domain Terpercaya di Indonesia",
+  description:
+    "Penyedia layanan hosting terbaik di Indonesia dengan uptime 99.9%, domain murah, VPS performa tinggi, dan solusi website untuk semua kebutuhan bisnis Anda.",
+  path: "/",
+  keywords: [
+    "vps hosting",
+    "cloud hosting",
+    "minecraft hosting",
+    "jasa pembuatan website",
+    "domain murah",
+    "vps digital ocean",
+  ],
 });
 
 export { viewport };
